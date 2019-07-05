973f15ef71845ee32613de4308159fe048e6d22c
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-07-05 09:47:53 +0200

    add night mode button

---
+++ src/views/SigninFlow/Recover.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'Recover',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/SigninFlow/Request.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'Request',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
